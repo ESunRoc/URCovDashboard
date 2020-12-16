5b24823ce63593be71c435165ac8a56894f64095
Eli Sun <elisun@icloud.com>
2020-12-15 19:28:42 -0500

    Add single installer/loader for required libraries

---
+++ loaddepends.r
@@ -0,0 +1,10 @@
+devtools::install_github("jakesherman/easypackages")
+library("easypackages")
+my_packages <- c("readxl", "tidyverse", "zoo", "RcppRoll",
+                 "gridExtra", "grid", "plotly", "htmlwidgets")
+packages(my_packages)
+libraries(my_packages)
+
+
+
+  
