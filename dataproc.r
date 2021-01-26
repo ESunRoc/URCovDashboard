700f4a3a01e9c1b31ee41c1b0c2c9164a7ec981d
Eli Sun <elisun@icloud.com>
2021-01-26 13:48:53 -0500

    1/26

--- dataproc.r
+++ dataproc.r
@@ -5,6 +5,7 @@ data$tottests <- cumsum(data$Tests)
 data$rolltests <- rollmean(data$Tests, k=7, fill=F, align="right")
 data$rollpos <- rollmean(data$Positive, k=7, fill=F, align="right")
 data$rolldaily.prate <- rollmean(data$daily.prate, k=7, fill=NA, align="right")
+data$daynum <- seq(1, length(data$Date))
 n.colfunc <- function(df, n=7, func){
   aggregate(x = df,
             by = list(gl(ceiling(nrow(df)/n), n)[1:nrow(df)]),
